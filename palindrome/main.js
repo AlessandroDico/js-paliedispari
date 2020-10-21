661e44fc01bcc50a9366b00b3f46128061d2967b
Ale <alessandrochb@gmail.com>
2020-10-21 15:13:37 +0200

    creato file + percorso logico parola palindroma

---
+++ palindrome/main.js
@@ -0,0 +1,13 @@
+// console.log('hello');
+
+/*
+Palindroma: scrivere una funzione per valutare se una parola è palindroma oppure no
+*/
+
+// PRENDO UNA PAROLA CHE è UNA STRINGA es. PAROLA
+
+// LA RENDO UN ARRAY  es. P  A  R  O  L  A
+
+// INVERTO GLI ARRAY (ogni singola lettera è un array ora)  A  L  O  R  A  P
+
+// CONFRONTO SE è UGUALE A PRIMA (PALINDROMA) O è DIVERSA
